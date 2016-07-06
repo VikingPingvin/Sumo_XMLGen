a9eaef7f6c1c71b2189bf1db634ac161a1d8fd7c
Daniel Schreiber <sch1458@gmail.com>
2016-07-06 17:07:08 +0200

    Init Commit

---
+++ VehicleXmlGenerator/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace VehicleXmlGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dp_min = new System.Windows.Forms.Label();
+            this.Dp_max = new System.Windows.Forms.Label();
+            this.rt_max = new System.Windows.Forms.Label();
+            this.Rt_min = new System.Windows.Forms.Label();
+            this.tb_dp_min = new System.Windows.Forms.TextBox();
+            this.tb_dp_max = new System.Windows.Forms.TextBox();
+            this.tb_rt_min = new System.Windows.Forms.TextBox();
+            this.tb_rt_max = new System.Windows.Forms.TextBox();
+            this.tb_num = new System.Windows.Forms.TextBox();
+            this.vehiclenum = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // Dp_min
+            // 
+            this.Dp_min.AutoSize = true;
+            this.Dp_min.Location = new System.Drawing.Point(29, 32);
+            this.Dp_min.Name = "Dp_min";
+            this.Dp_min.Size = new System.Drawing.Size(58, 13);
+            this.Dp_min.TabIndex = 0;
+            this.Dp_min.Text = "Depart min";
+            // 
+            // Dp_max
+            // 
+            this.Dp_max.AutoSize = true;
+            this.Dp_max.Location = new System.Drawing.Point(26, 54);
+            this.Dp_max.Name = "Dp_max";
+            this.Dp_max.Size = new System.Drawing.Size(61, 13);
+            this.Dp_max.TabIndex = 1;
+            this.Dp_max.Text = "Depart max";
+            this.Dp_max.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // rt_max
+            // 
+            this.rt_max.AutoSize = true;
+            this.rt_max.Location = new System.Drawing.Point(230, 54);
+            this.rt_max.Name = "rt_max";
+            this.rt_max.Size = new System.Drawing.Size(59, 13);
+            this.rt_max.TabIndex = 3;
+            this.rt_max.Text = "Route Max";
+            this.rt_max.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Rt_min
+            // 
+            this.Rt_min.AutoSize = true;
+            this.Rt_min.Location = new System.Drawing.Point(230, 32);
+            this.Rt_min.Name = "Rt_min";
+            this.Rt_min.Size = new System.Drawing.Size(56, 13);
+            this.Rt_min.TabIndex = 2;
+            this.Rt_min.Text = "Route Min";
+            this.Rt_min.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tb_dp_min
+            // 
+            this.tb_dp_min.Location = new System.Drawing.Point(103, 24);
+            this.tb_dp_min.Name = "tb_dp_min";
+            this.tb_dp_min.Size = new System.Drawing.Size(100, 20);
+            this.tb_dp_min.TabIndex = 4;
+            // 
+            // tb_dp_max
+            // 
+            this.tb_dp_max.Location = new System.Drawing.Point(103, 51);
+            this.tb_dp_max.Name = "tb_dp_max";
+            this.tb_dp_max.Size = new System.Drawing.Size(100, 20);
+            this.tb_dp_max.TabIndex = 5;
+            // 
+            // tb_rt_min
+            // 
+            this.tb_rt_min.Location = new System.Drawing.Point(333, 24);
+            this.tb_rt_min.Name = "tb_rt_min";
+            this.tb_rt_min.Size = new System.Drawing.Size(100, 20);
+            this.tb_rt_min.TabIndex = 6;
+            // 
+            // tb_rt_max
+            // 
+            this.tb_rt_max.Location = new System.Drawing.Point(333, 51);
+            this.tb_rt_max.Name = "tb_rt_max";
+            this.tb_rt_max.Size = new System.Drawing.Size(100, 20);
+            this.tb_rt_max.TabIndex = 7;
+            // 
+            // tb_num
+            // 
+            this.tb_num.Location = new System.Drawing.Point(680, 24);
+            this.tb_num.Name = "tb_num";
+            this.tb_num.Size = new System.Drawing.Size(100, 20);
+            this.tb_num.TabIndex = 8;
+            // 
+            // vehiclenum
+            // 
+            this.vehiclenum.AutoSize = true;
+            this.vehiclenum.Location = new System.Drawing.Point(607, 24);
+            this.vehiclenum.Name = "vehiclenum";
+            this.vehiclenum.Size = new System.Drawing.Size(61, 13);
+            this.vehiclenum.TabIndex = 9;
+            this.vehiclenum.Text = "vehiclenum";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.CheckFileExists = true;
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 416);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vehiclenum);
+            this.Controls.Add(this.tb_num);
+            this.Controls.Add(this.tb_rt_max);
+            this.Controls.Add(this.tb_rt_min);
+            this.Controls.Add(this.tb_dp_max);
+            this.Controls.Add(this.tb_dp_min);
+            this.Controls.Add(this.rt_max);
+            this.Controls.Add(this.Rt_min);
+            this.Controls.Add(this.Dp_max);
+            this.Controls.Add(this.Dp_min);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Dp_min;
+        private System.Windows.Forms.Label Dp_max;
+        private System.Windows.Forms.Label rt_max;
+        private System.Windows.Forms.Label Rt_min;
+        private System.Windows.Forms.TextBox tb_dp_min;
+        private System.Windows.Forms.TextBox tb_dp_max;
+        private System.Windows.Forms.TextBox tb_rt_min;
+        private System.Windows.Forms.TextBox tb_rt_max;
+        private System.Windows.Forms.TextBox tb_num;
+        private System.Windows.Forms.Label vehiclenum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ VehicleXmlGenerator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VehicleXmlGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            Int32.TryParse(tb_num.Text, out num);
+            int rtmin, rtmax, dpmin, dpmax;
+            rtmin = Convert.ToInt32(tb_rt_min.Text);
+            rtmax = Convert.ToInt32(tb_rt_max.Text);
+            dpmin = Convert.ToInt32(tb_dp_min.Text);
+            dpmax = Convert.ToInt32(tb_dp_max.Text);
+
+            Random r = new Random();
+            int rtnum = r.Next(rtmin, rtmax);
+            int dpnum = r.Next(dpmin, dpmax);
+            int id = 0;
+
+            //<vehicle depart="1" id="veh0" route="route0" type="Car" />
+            System.Text.StringBuilder sb = new StringBuilder();
+           
+            
+            for (int i = 0;i< num; i++)
+            {
+                rtnum = r.Next(rtmin, rtmax);
+                dpnum = r.Next(dpmin, dpmax);
+                sb.Append("<vehicle depart=\"");
+                sb.Append(dpnum.ToString());
+                sb.Append("\" id=\"veh");
+                sb.Append(id.ToString());
+                id++;
+                sb.Append("\" route=\"route");
+                sb.Append(rtnum.ToString());
+                sb.Append("\" type=\"Car\"");
+                sb.Append(" />");
+                sb.Append(System.Environment.NewLine);
+             
+            }
+
+            SaveFileDialog savefiledialog = new SaveFileDialog();
+            savefiledialog.Filter = "TextFile|*.txt";
+            savefiledialog.Title = "Save To File";
+            if(savefiledialog.ShowDialog()== DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(savefiledialog.FileName))
+                    sw.WriteLine(sb.ToString());
+            }
+            
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
